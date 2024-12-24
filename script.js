916db60e585a3c52118208d0a8c0c2b6a3aedd42
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-24 21:35:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ inputs.forEach((input,index)=>{
 	input.addEventListener('keydown', (e)=>{
 		if(e.key==='Backspace' && input.value===''){
 			if(index>0){
+				inputs[index].classList.remove('focused');
+				inputs[index-1].classList.add('focused');
 				inputs[index-1].focus();
 			}
 		}
