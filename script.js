d3aced61889fe42a44935c98a96bb39023cd57d2
Shiva Kumar Karru <103453725+Shivaksk119@users.noreply.github.com>
2024-12-24 20:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+let inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input,index)=>{
+	input.addEventListener('input',()=>{
+		if(input.value !=''){
+			if(index<inputs.length-1){
+				inputs[index+1].focus();
+			}
+		}
+	})
+
+	input.addEventListener('keydown', (e)=>{
+		if(e.key==='Backspace' && input.value===''){
+			if(index>0){
+				inputs[index-1].focus();
+			}
+		}
+	})
+})
